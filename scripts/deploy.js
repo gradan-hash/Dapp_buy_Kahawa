635668f7b95d4efbcf43cdaa905b44434dce73b8
gradan-hash <mutukunyaa@gmail.com>
2023-08-24 16:16:04 +0300

    structure

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const kahawa = await hre.ethers.deployContract("kahawa");
+  const kahawa = await hre.ethers.deployContract("kahawa"); //fetching bytecode and ABI
 
-  await kahawa.waitForDeployment();
+  await kahawa.waitForDeployment(); // instance and deploy
 
-  console.log(`ETH  deployed to,  ${kahawa.address}`);
+  console.log("ETH  deployed to", `${kahawa.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
